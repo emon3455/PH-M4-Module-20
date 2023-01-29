2cbd5cdfa34b49fbc3aa82c14cc0beefdb794f59
emon3455 <emon.mhk69@gmail.com>
2023-01-30 00:57:05 +0600

    practice problem5 done

---
+++ practice-problem5.js
@@ -0,0 +1,12 @@
+const pizza = {
+
+    toppings: ['cheese', 'sauce', 'pepperoni'],
+   
+    crust: 'deep dish',
+   
+    serves: 2
+   
+}
+console.log(pizza);
+var item = pizza.toppings;
+console.log(item[2]);
