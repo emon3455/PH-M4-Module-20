87fb8bdc19e0f646ef6cd287616e0e12e43005d0
emon3455 <emon.mhk69@gmail.com>
2023-01-30 00:23:36 +0600

    properties of object

---
+++ object.js
@@ -0,0 +1,17 @@
+const emon = {
+    name: "Emon",
+    age: 21,
+    gender: "Male",
+    height: 5.10
+
+}
+
+const car = {
+    brand: "Audi",
+    model: "v2",
+    color: "black",
+    price: 16540000
+}
+
+console.log(emon);
+console.log(emon.age);
---
+++ propertiesOfObject.js
@@ -0,0 +1,17 @@
+const shoping= {
+    mobile: 2,
+    pc: 1,
+    keyboard: 3,
+    mouse: 1,
+    pen: 20,
+    noteBook: 10
+}
+console.log(shoping);
+
+// all the keys of object:
+var keys =  Object.keys(shoping);
+console.log(keys);
+
+// all the values of object:
+var values =  Object.values(shoping);
+console.log(values);
