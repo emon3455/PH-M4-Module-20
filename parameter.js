5b88385769cc263a75f98c32253492f237c5a2c5
emon3455 <emon.mhk69@gmail.com>
2023-01-29 23:09:05 +0600

    function with parameter done

---
+++ parameter.js
@@ -0,0 +1,8 @@
+// function with parameter:
+function sum(num1 , num2){
+    console.log("Number-1:",num1,"Number-2:",num2);
+    var sum = num1+num2;
+    console.log("The sum of",num1,"and",num2 ,":",sum);
+}
+
+sum(15,25);
