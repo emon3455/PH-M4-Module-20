5c784d3b79542d3d483ea1fe306a13c50a70d325
emon3455 <emon.mhk69@gmail.com>
2023-01-30 10:15:54 +0600

    problem-3 done

---
+++ problem3.js
@@ -0,0 +1,9 @@
+function fullName (firstName , lastName){
+    let full =  firstName + " " + lastName;
+    return full;
+}
+
+let firstName = "Ariyan";
+let lastName = "Emon";
+var fulltext= fullName(firstName,lastName);
+console.log(fulltext);
