abe08ee5baa5f13b9379df5acfebbd3c397f48e5
emon3455 <emon.mhk69@gmail.com>
2023-01-30 09:57:23 +0600

    problem-2 done

---
+++ problem2.js
@@ -0,0 +1,8 @@
+function casetranfer(text){
+    var upperconvert = text.toUpperCase();
+    return upperconvert;
+}
+
+var message = "I love bangladesh";
+var converted = casetranfer(message);
+console.log(converted);
