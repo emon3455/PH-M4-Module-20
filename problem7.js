fabae594cebf99da7053336e70c6e632ce2f0f02
emon3455 <emon.mhk69@gmail.com>
2023-01-30 10:30:16 +0600

    problem-5 done

---
+++ problem7.js
@@ -0,0 +1,13 @@
+function evenOdd (number){
+    
+    if(num%2 == 0){
+        console.log("Even Number");
+    }
+    else{
+        console.log("Odd Number");
+    }
+
+}
+
+let num = 3;
+evenOdd(num);
